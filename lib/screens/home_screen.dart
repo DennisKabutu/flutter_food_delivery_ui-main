4ae30045a221842d123c7c93d1d4696caf2c2be3
kim <denoh6804@gmail.com>
2021-12-03 10:14:52 +0300

    Nullability added in data files as per Dart 2.0

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_ui/data/data.dart';
+import 'package:flutter_food_delivery_ui/widgets/recent_orders.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({key}) : super(key: key);
@@ -9,7 +10,6 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
@@ -17,54 +17,50 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('Flutter Food Delivery'),
         leading: IconButton(
-          onPressed: (){},
+          onPressed: () {},
           icon: Icon(Icons.account_circle_sharp),
           iconSize: 30.0,
         ),
         actions: [
           TextButton(
-              onPressed: (){},
+              onPressed: () {},
               child: Text(
-                  'cart (${currentUser.cart.length})',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0
-                ),
+                'cart (${currentUser.cart.length})',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
               )
           )
         ],
       ),
-
-
       body: ListView(
         children: [
-          TextField(
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.symmetric(vertical: 15.0),
-              fillColor: Colors.white,
-              hintText: 'Search for food or Restaurants',
-              prefixIcon: Icon(
-                Icons.search,
-                size: 30.0,
-              ),
-              suffixIcon: IconButton(
-                icon: Icon(Icons.clear),
-                onPressed: (){},
-              ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30.0),
-                borderSide: BorderSide(width: 0.8)
-              ),
-              enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                  borderSide: BorderSide(
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                  fillColor: Colors.white,
+                  hintText: 'Search for food or Restaurants',
+                  prefixIcon: Icon(
+                    Icons.search,
+                    size: 30.0,
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {},
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                      borderSide: BorderSide(width: 0.8)),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(
                       width: 0.8,
                       color: Theme.of(context).primaryColor,
-                  ),
-
-              )
+                    ),
+                  )),
             ),
-          )
+          ),
+          RecentOrders()
         ],
       ),
     );
